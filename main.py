cc754675339178ed6e9f8bbe69d9868e0d89b01f
Anas <112882511+anasm20@users.noreply.github.com>
2023-06-08 18:04:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,29 @@
-import nltk
-from flask import request
-from flask import jsonify
-from flask import Flask, render_template
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import nltk  # Importing the nltk library for natural language processing
+from flask import request, jsonify, Flask, render_template  # Importing necessary modules and classes from Flask
+from nltk.sentiment.vader import SentimentIntensityAnalyzer  # Importing the SentimentIntensityAnalyzer class from nltk.sentiment.vader
 
+app = Flask(__name__)  # Creating a Flask application object named 'app'
 
-app = Flask(__name__)
-
-@app.route('/')
+@app.route('/')  # Route decorator that binds the root URL ('/') to the 'my_form' function
 def my_form():
-    return render_template('index.html')
+    return render_template('index.html')  # Render the 'index.html' template and return it as the response
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['POST'])  # Route decorator that binds the root URL ('/') with the HTTP method POST to the 'my_form_post' function
 def my_form_post():
-    text = request.form['text']
-    nltk.download('vader_lexicon')
-    sid = SentimentIntensityAnalyzer()
-    score = ((sid.polarity_scores(str(text))))['compound']
+    text = request.form['text']  # Retrieve the text submitted in the form
+    nltk.download('vader_lexicon')  # Download the lexicon required for sentiment analysis
+    sid = SentimentIntensityAnalyzer()  # Create an instance of the SentimentIntensityAnalyzer class
+    score = sid.polarity_scores(str(text))['compound']  # Perform sentiment analysis and extract the compound score
 
-    if(score > 0):
+    if score > 0:  # Determine if the sentiment is positive
         label = 'This sentence is positive 😁'
-    elif(score == 0):
+    elif score == 0:  # Determine if the sentiment is neutral
         label = 'This sentence is neutral 😐'
-    else:
+    else:  # The sentiment is negative
         label = 'This sentence is negative 😕'
 
-    return(render_template('index.html', variable=label))
+    return render_template('index.html', variable=label)  # Render the 'index.html' template and pass the 'label' variable as a parameter
 
 if __name__ == "__main__":
-    app.run(port='8088', threaded=False, debug=True)
+    app.run(port='8088', threaded=False, debug=True)  # Start the Flask development server on port 8088, with debugging mode enabled
+
