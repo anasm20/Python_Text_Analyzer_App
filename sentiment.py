e1dc9a70a0c2906d80a48570e8555b0b26d08620
Anas <112882511+anasm20@users.noreply.github.com>
2023-06-08 18:05:46 +0200

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -1,19 +1,22 @@
-#install libraries: nltk, flask
+# Install libraries: nltk, flask
 
-import nltk
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import nltk  # Importing the nltk library for natural language processing
+from nltk.sentiment.vader import SentimentIntensityAnalyzer  # Importing the SentimentIntensityAnalyzer class from nltk.sentiment.vader
 
 
-text = "today is a very lucky day"
-nltk.download('vader_lexicon')
-sid = SentimentIntensityAnalyzer()
-score = ((sid.polarity_scores(str(text))))['compound'] #The normalized compound score which calculates the sum of all lexicon ratings and takes values from -1 to 1
+text = "today is a very lucky day"  # The text for sentiment analysis
 
-if(score > 0):
+nltk.download('vader_lexicon')  # Download the lexicon required for sentiment analysis
+
+sid = SentimentIntensityAnalyzer()  # Create an instance of the SentimentIntensityAnalyzer class
+
+score = sid.polarity_scores(str(text))['compound']  # Perform sentiment analysis and extract the compound score
+
+if score > 0:  # Determine if the sentiment is positive
     label = 'The sentiment in the sentence is positive'
-elif(score == 0):
+elif score == 0:  # Determine if the sentiment is neutral
     label = 'The sentiment in the sentence is neutral'
-else:
+else:  # The sentiment is negative
     label = 'The sentiment in the sentence is negative'
 
-print(label)
+print(label)  # Print the sentiment label
